d755962031558ea490118bbe6873fc9b0bd577f3
FrankFranken <Frankfranken1985@gmail.com>
2017-02-24 10:05:20 -0600

    added tips

--- Image Analysis/Models/EmailFormModel.cs
+++ Image Analysis/Models/EmailFormModel.cs
@@ -8,9 +8,9 @@ namespace Image_Analysis.Models
 {
     public class EmailFormModel
     {
-        [Required, Display(Name = "Your name")]
+        [Required, Display(Name = "name")]
         public string FromName { get; set; }
-        [Required, Display(Name = "Your email"), EmailAddress]
+        [Required, Display(Name = "email"), EmailAddress]
         public string FromEmail { get; set; }
         [Required]
         public string Message { get; set; }
